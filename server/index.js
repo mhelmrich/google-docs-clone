eb074f27256cff66148c7752b4d54d7f3cb4a109
mhelmrich <33710207+mhelmrich@users.noreply.github.com>
2018-08-06 15:27:41 -0700

    add passport

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser((user, done) => done(null, user._id));
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => done(err, user));
+});
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({username: username}, (err, user) => {
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    if (user.password !== password) return done(null, false);
+    return done(null, user);
+  });
+}));
+
+server.use(passport.initialize());
+server.use(passport.session());
 
 io.on('connection', (socket) => {
  console.log('connected', socket);
