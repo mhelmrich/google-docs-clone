ee64121e45a51a389175dcb39b21aeb48b8796f9
mhelmrich <33710207+mhelmrich@users.noreply.github.com>
2018-08-06 16:07:03 -0700

    add authentication routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-const User = require('./models');
+const User = require('./models/User.js');
+const auth = require('./routes/auth');
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to database!');
 });
 mongoose.connect(process.env.MONGODB_URI);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({secret: process.env.SECRET}));
 
 passport.serializeUser((user, done) => done(null, user._id));
 
@@ -28,6 +36,8 @@ passport.use(new LocalStrategy((username, password, done) => {
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/', auth(passport));
 
 io.on('connection', (socket) => {
  console.log('connected', socket);
--- server/models/User.js
+++ server/models/User.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true
+    required: true,
   },
   username: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
